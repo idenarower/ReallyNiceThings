7d15a6d5d2475953882ab7d27afc99472366dd9d
Dashek <daszkiem@gmail.com>
2023-02-27 18:48:02 +0100

    Git init

---
+++ app.py
@@ -0,0 +1,26 @@
+# from flask import Flask, render_template
+
+# app=Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     return render_template("index.html")
+
+# @app.route('/ua')
+# def ua():
+#     return render_template("ua.html")
+
+# @app.route('/de')
+# def de():
+#     return render_template("de.html")
+
+# @app.route('/pl')
+# def pl():
+#     return render_template("pl.html")
+
+# @app.route('/us')
+# def us():
+#     return render_template("us.html")
+
+# if __name__=="__main__":
+#     app.run()
---
+++ readme.md
@@ -0,0 +1,5 @@
+Books, fairy tales and stories for children and teens in Ukrainian for free. Click on the picture and download the pdf on your device.
+
+All books have been made available for free by the Osvitoria Foundation
+
+Feel free with use it. Just let me know if its possible.
